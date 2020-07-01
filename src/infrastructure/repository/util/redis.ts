20e08884b33a73d69c35c8e1d5605040b4365885
douglasdrdc@gmail.com <douglasdrdc@gmail.com>
2020-07-01 01:02:39 -0300

    Included external connnnection redis

--- src/infrastructure/repository/util/redis.ts
+++ src/infrastructure/repository/util/redis.ts
@@ -1,5 +1,8 @@
 import * as Redis from 'ioredis';
 
 const redisUrl: string = process.env.REDIS_URL || 'redis://localhost:6379';
+const redisPasswork: string = process.env.REDIS_PASSWORD || null;
 
-export default new Redis(redisUrl);
+export default !!redisPasswork ?
+    new Redis(redisUrl, { password: redisPasswork }) : 
+    new Redis(redisUrl);
